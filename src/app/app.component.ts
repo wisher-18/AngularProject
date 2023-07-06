a7253862ef1f773ff02ee41f87e9699ddbeb20e4
Wisher <bhushanpagare36@gmail.com>
2023-07-06 15:29:35 +0530

    Some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularProject';
+onSubmit(_t5: NgForm) {
+throw new Error('Method not implemented.');
+}
+
+  title = 'Template driven forms';
+ 
+  countryList:country[] = [
+    new country("1", "India"),
+    new country('2', 'USA'),
+    new country('3', 'England')
+  ];
+}
+ 
+export class country {
+  id:string;
+  name:string;
+ 
+  constructor(id:string, name:string) {
+    this.id=id;
+    this.name=name;
+  }
+
 }
